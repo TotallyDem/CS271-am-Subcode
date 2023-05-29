d8bed0ca8e8652d1842195919af722252b4217bc
TotallyDem <alexmurray308@gmail.com>
2023-05-29 12:28:04 -0700

    Multiplication program completed
    Finally did the program after weeks of procrastinating.

--- assignment4/04/mult/Mult.asm
+++ assignment4/04/mult/Mult.asm
@@ -10,3 +10,26 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+(START)
+	// Initialize product to 0.
+	@R2
+	M=0
+(LOOP)
+	@R1
+	D=M
+	@END
+	D;JLE // If R1 <= 0 stop
+	@R0
+	D=M
+	@R2
+	M=M+D // Add R0 to R2
+	@R1
+	M=M-1 // R1 -= 1
+	// Loop
+	@LOOP
+	0;JMP
+(END)
+	// stop
+	@END
+	0;JMP
