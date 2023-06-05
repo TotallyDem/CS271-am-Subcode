dc9aec2f0fbc6c11e4ccf6b301714ef99df1fb86
TotallyDem <alexmurray308@gmail.com>
2023-06-05 00:29:01 -0700

    Finish project 4
    Yes, I am disappointed in myself for taking this long to complete it.

--- assignment4/04/fill/Fill.asm
+++ assignment4/04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// A = adress
+// M = Stored data (at adress)
+// D = Memory
+
+//Scren starts at 16384 and ends at 24575	
+(BEGIN)
+(CHECK_KEYBOARD)
+	// Set current to first pixel
+	@16384
+	D=A
+	@current
+	M=D
+	// If keyboard is pressed set fill
+	@KBD
+	D=M
+	@fillvalue
+	M=-1
+	@DRAW
+	D;JNE
+	// Set empty
+	@fillvalue
+	M=0
+(DRAW)
+	// Draw based on previous
+	@fillvalue
+	D=M
+	@current
+	A=M
+	M=D
+	// Jump back if finished drawing
+	@current
+	D=M
+	@24575
+	D=D-A
+	@CHECK_KEYBOARD
+	D;JGE
+	// Advance pixel
+	@current
+	M=M+1
+	// Continue drawing
+	@DRAW
+	0;JMP
